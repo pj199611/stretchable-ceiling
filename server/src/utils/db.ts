fc3692cf1c53847f71b9550b4ff1d9d10a5d9d39
MrTacoMan <43774441+pj199611@users.noreply.github.com>
2024-11-03 14:02:47 +0530

    db name changed

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default function setupDB() {
   mongoose
-    .connect(process.env.MONGO_URI!)
+    .connect(process.env.MONGO_URI!,{dbName:"strechable-celing"})
     .then(() => console.log('Connected to DB'))
     .catch((err) => console.log('DB connection error:', err));
 }
