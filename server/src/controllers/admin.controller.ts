30cc61735be5f027e6a4f6908f830ebd95708a1d
MrTacoMan <43774441+pj199611@users.noreply.github.com>
2024-11-04 17:07:52 +0530

    get user

--- server/src/controllers/admin.controller.ts
+++ server/src/controllers/admin.controller.ts
@@ -29,6 +29,23 @@ export const deleteUser = async (
   }
 };
 
+export const getUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting user', error });
+  }
+};
+
+
 export const updateUser = async (
   req: Request,
   res: Response
--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -5,12 +5,14 @@ import {
   getAllUsers,
   deleteUser,
   updateUser,
+  getUser,
 } from '../controllers/admin.controller';
 
 const router = express.Router();
 
 // user management routes
 router.get('/users', authenticateToken, checkAdminRole, getAllUsers);
+router.get('/users/:id', authenticateToken, checkAdminRole, getUser);
 router.delete('/users/:id', authenticateToken, checkAdminRole, deleteUser);
 router.put('/users/:id', authenticateToken, checkAdminRole, updateUser);
 
