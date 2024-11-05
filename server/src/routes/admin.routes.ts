9e451a0e1c135ca863e63c5a92eb9aa1510092fe
prabhjot <sprabhjot768@gmail.com>
2024-11-05 17:23:16 +0530

    minor routes changes

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -17,7 +17,7 @@ import {
   deleteProduct,
 } from '../controllers/products.controller';
 
-import { getAllOrdersForAdmins,getOrderById,updateOrder } from '../controllers/order.controller';
+import { getAllOrdersForAdmins } from '../controllers/order.controller';
 
 const router = express.Router();
 
@@ -31,8 +31,6 @@ router.delete('/products/:id',authenticateToken, checkAdminRole, deleteProduct);
 
 // Order Routes
 router.get('/orders',authenticateToken, checkAdminRole, getAllOrdersForAdmins);
-router.get('/orders/:id',authenticateToken, checkAdminRole, getOrderById);
-router.put('/orders/:id', authenticateToken,checkAdminRole,updateOrder);
 
 // user management routes
 router.get('/users', authenticateToken, checkAdminRole, getAllUsers);
