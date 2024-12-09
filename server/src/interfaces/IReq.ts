bf1bf4c41fa9d0092572cdc7f9c5d35f642beaa2
prabhjot singh <sprabhjot768@gmail.com>
2024-12-09 16:10:24 +0530

    changes

--- server/src/interfaces/IReq.ts
+++ server/src/interfaces/IReq.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express';
-import IUser from '../interfaces/IUser';
+import IUser from '../interfaces/Iuser';
 
 export interface IRequest extends Request {
   user?: IUser | null;
