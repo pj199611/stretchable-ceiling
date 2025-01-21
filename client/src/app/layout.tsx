b7ca30908196e37a32c090fad4404ab97a90a336
Aman Gupta <gupta.amangupta98@gmail.com>
2025-01-21 22:54:22 +0530

    GA

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -12,6 +12,7 @@ import SettingsProvider from "@/contexts/SettingContext";
 import TokenContext from "@/contexts/TokenContext";
 import ProductProvider from "@/contexts/ProductContext";
 import ProgressBar from "@/components/progress";
+import Script from "next/script";
 
 // IMPORT i18n SUPPORT FILE
 // import "i18n";  // unknown issue???
@@ -19,6 +20,22 @@ import ProgressBar from "@/components/progress";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-JZHKYBMWHG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {` 
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-JZHKYBMWHG');
+          `}
+        </Script>
+      </head>
       <body className={openSans.className}>
         <UserContext>
           <CartProvider>
