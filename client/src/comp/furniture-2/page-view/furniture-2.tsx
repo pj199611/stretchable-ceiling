a55e84bf90e794b9f586ced9688b65d95631b264
Aman Gupta <gupta.amangupta98@gmail.com>
2025-01-29 15:09:13 +0530

    Remove Unwanted Files

---
+++ client/src/comp/furniture-2/page-view/furniture-2.tsx
@@ -0,0 +1,45 @@
+import Section1 from "../section-1";
+import Section2 from "../section-2";
+import Section7 from "../section-7";
+import Section9 from "../section-9";
+import WhatsApp from "@/images/icons/Whatsapp";
+
+export default function FurnitureTwoPageView() {
+  return (
+    <div className="bg-white pt-2">
+      {/* TOP BANNER SECTION */}
+      <Section1 />
+
+      {/* BANNER GRID SECTION */}
+      <Section2 />
+
+      {/* SERVICES SECTION */}
+      <Section9 />
+
+      {/* TESTIMONIAL SECTION */}
+      <Section7 />
+
+      <a
+        href={"https://wa.me/8447030606"}
+        target="_blank"
+        rel="noreferrer noopenner"
+        key={"whatsapp-Icon"}
+      >
+        <div
+          style={{
+            position: "fixed",
+            bottom: 0,
+            right: 0,
+            zIndex: 5,
+            // borderRadius: 50,
+            // backgroundColor: "#00d7186b",
+            // padding: "0.5em 0.6em",
+            margin: "1em",
+          }}
+        >
+          <WhatsApp style={{ width: "2.25em", height: "2.25em" }} />
+        </div>
+      </a>
+    </div>
+  );
+}
