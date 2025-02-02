77b3848ce20fc9cb85fcbeee2c2d8166a1dde07a
Aman Gupta <gupta.amangupta98@gmail.com>
2025-02-02 23:25:55 +0530

    Minor Fixes

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    theme: "DEFAULT",
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
