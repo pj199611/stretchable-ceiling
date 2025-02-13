b1089a31d6d68e6a484114a0f7c7b70e00e56fb8
Aman Gupta <gupta.amangupta98@gmail.com>
2025-02-13 22:32:20 +0530

    Add LinkedIn Link

--- client/src/components/footer/components/social-links.tsx
+++ client/src/components/footer/components/social-links.tsx
@@ -4,21 +4,31 @@ import Youtube from "@/images/icons/Youtube";
 import Facebook from "@/images/icons/Facebook";
 import Instagram from "@/images/icons/Instagram";
 import WhatsApp from "@/images/icons/Whatsapp";
+import LinkedIn from "@/images/icons/LinkedIn";
 import Gmail from "@/images/icons/Gmail";
+// import GoogleMaps from "@/images/icons/GoogleMaps";
 
 export const SOCIAL_ICON_LINKS = [
-  { Icon: Facebook, url: "https://www.facebook.com/" },
-  { Icon: Twitter, url: "https://wa.me/8447041309" },
-  { Icon: WhatsApp, url: "https://wa.me/8447041309" },
-  {
-    Icon: Youtube,
-    url: "https://www.youtube.com/",
-  },
+  // { Icon: Facebook, url: "https://www.facebook.com/" },
+  // { Icon: Twitter, url: "" },
+  // {
+  //   Icon: Youtube,
+  //   url: "https://www.youtube.com/",
+  // },
   {
     Icon: Instagram,
     url: "https://www.instagram.com/nestandnookinterior/",
   },
-  // { Icon: Gmail, url: "contact@nestandnookinterior.com" },
+  {
+    Icon: LinkedIn,
+    url: "https://www.linkedin.com/company/nest-nook-interior/",
+  },
+  { Icon: WhatsApp, url: "https://wa.me/8447041309" },
+  {
+    Icon: Gmail,
+    url: "mailto:contact@nestandnookinterior.com?subject=Hello%20There&body=I%20hope%20this%20message%20finds%20you%20well.",
+  },
+  // { Icon: GoogleMaps, url: "" },
 ];
 
 export default function SocialLinks() {
